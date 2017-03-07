79c0c6c9bb6bd02d1868e6d7d07c836c3f8c8f8f
Austin Zadoks <zooks97@gmail.com>
2017-03-07 01:13:25 -0500

    motherfucking magic

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,15 +1,19 @@
 #include "../libs/motion.h"
 #include "../libs/data_cleaning.h"
 
+//28.4 = 30
+
 ////PID straight test v1
 task main(){
-    float pow = 50;
-    //straight(609.6, pow);
-    bFloatDuringInactiveMotorPWM = true;
-    zeroTurn(30, pow/2);
-    bFloatDuringInactiveMotorPWM = false;
+    float pow = 80;
     motor[motorA] = 0;
     motor[motorB] = 0;
-    wait1Msec(500);
+    zeroTurn(28.4, pow/2);
+    motor[motorA] = 0;
+    motor[motorB] = 0;
+    wait1Msec(100);
+    straight(1000, pow);
+    displayTextLine(1, "%d", nImmediateBatteryLevel);
+    wait1Msec(100);
     bFloatDuringInactiveMotorPWM = true;
 }
