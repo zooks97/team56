26be2631b202e92733b7e5a92461485eaa4d79df
cregast <stevecreger@hotmail.com>
2017-03-06 20:29:57 -0500

    Batery scaling test
    Tests if the battery scaling function will work

--- src/bin/main.c
+++ src/bin/main.c
@@ -5,11 +5,12 @@ straight(10000);
 
 }
 
-//#include "motion.h"
+#include "motion.h"
+#include "data_cleaning.h"
 
 ////PID straight test v1
-//task main(){
-//    //float distance = 3000; //mm
+task main(){
+   float distance = 3000; //mm
 //    float angle = 40; //degrees
 //    zeroTurn(angle);
 //    motor[motorA] = 0;
@@ -17,5 +18,6 @@ straight(10000);
 //    displayString(1, "%f", nImmediateBatteryLevel);
 //    wait1Msec(1000);
 //    //straight(distance);
-
+    speed = batToVel(nImmediateBatteryLevel);
+    straight(distance, speed);
 //}
