c7f85588b0a2b612354403df41a38e9340304cf2
Austin Zadoks <zooks97@gmail.com>
2017-03-11 15:03:57 -0500

    clean main for demonstration

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,25 +1,12 @@
+/**** TEAM 56 ****/
+// Demonstration //
+
 #include "../libs/motion.h"
 #include "../libs/data.h"
 #include "../libs/lsts.h"
 #include "../libs/bin.h"
+#include "../libs/sensor.h"
 
-//28.4 = 30
-
-////PID straight test v1
 task main(){
-    float pow = 70;
-    motor[motorA] = 0;
-    motor[motorB] = 0;
 
-    zeroTurn(28.4, pow/2);
-
-    motor[motorA] = 0;
-    motor[motorB] = 0;
-    wait1Msec(100);
-
-    straight(1200, pow);
-
-    displayTextLine(1, "%d", nImmediateBatteryLevel);
-    wait1Msec(100);
-    bFloatDuringInactiveMotorPWM = true;
 }
